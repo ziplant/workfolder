9a036fb3bad6361041eb9abc965d8e7ad9cc5bc4
ZipLant <gg.ziplant@gmail.com>
2020-08-07 17:40:01 +0300

    add node cli create command

--- README.md
+++ README.md
@@ -2,7 +2,24 @@
 
 Simple gulp bulder using pug, sass, babel, webpack
 
-## install 
+## Install 
 ```cli
-npm install workfolder
+npm install workfolder -g
+```
+
+## Usage
+To create the project directory enter the following command:
+```cli
+workfolder create <name>
+```
+
+You have the following npm scripts:
+```json
+"start": "cross-env NODE_ENV=development gulp start",
+"build": "cross-env NODE_ENV=production gulp build",
+"example-component": "gulp addcomp -n example"
+```
+So to create new component enter:
+```json
+gulp addcomp -n <name>
 ```
---
+++ init.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const commander = require('commander')
+const package = require('./package.json')
+const fs = require('fs-extra')
+const path = require('path')
+const { exec } = require("child_process")
+
+commander.version(package.version).description(package.description)
+
+commander
+  .command('create <name>')
+  .description('Create project directory')
+  .action(async (name) => {
+    console.log('Copy files...')
+    await fs.copy(path.resolve(__dirname, 'project'), name)
+    console.log('Downloading node_modules...')
+    exec(`npm install --prefix ${name}`, (error) => {
+      if (error) {
+          console.log(`error: ${error.message}`)
+          return
+      }
+
+      console.log('Project created!')
+    });
+  })
+
+commander.parse(process.argv)
